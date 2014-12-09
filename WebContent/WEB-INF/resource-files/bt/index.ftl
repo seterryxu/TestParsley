e07a1826384e3640ac791183074e5f6dcb153365
seterryxy <cs.terryxu@gmail.com>
2014-12-09 19:33:34 +0800

    +1

---
+++ WebContent/WEB-INF/resource-files/bt/index.ftl
@@ -0,0 +1,12 @@
+<#include "/breadcrumb.ftl">
+<#include "/panel.ftl">
+<#include "/button.ftl">
+<#include "/well.ftl">
+
+<@breadcrumb />
+
+<@button name="refresh"/>
+
+${it}
+
+<@well/>
---
+++ src/testcases/TestDiscovery.java
@@ -0,0 +1,25 @@
+package testcases;
+
+import java.util.Enumeration;
+
+import org.apache.commons.discovery.tools.Service;
+import org.seterryxu.parsleyframework.core.Facet;
+
+public class TestDiscovery {
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ClassLoaders cl=new ClassLoaders();
+		// cl.put(Facet.class.getClassLoader());
+		// DiscoverClass c=new DiscoverClass(cl);
+		// c.find(spiClass)
+		Enumeration<Facet> en = Service.providers(Facet.class);
+
+		System.out.println(en.hasMoreElements());
+		while (en.hasMoreElements()) {
+			Facet f = en.nextElement();
+			System.out.println(f);
+		}
+	}
+}
